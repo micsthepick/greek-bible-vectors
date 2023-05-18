865d121352d4a28c400295f60633992e0d1b8d0d
micsthepick <11528421+micsthepick@users.noreply.github.com>
2023-05-18 17:35:46 +1000

    fasttext greek vects

--- prepare_fasttext.py
+++ prepare_fasttext.py
@@ -2,6 +2,7 @@ import os
 import glob
 import re
 import numpy as np
+from gensim import corpora
 from gensim.models import FastText
 from gensim.models.callbacks import CallbackAny2Vec
 from matplotlib import pyplot as plt
@@ -13,38 +14,33 @@ class callback(CallbackAny2Vec):
     def __init__(self):
         self.tq = tqdm(range(EPOCHS))
         self.progress = iter(self.tq)
-        self.last_loss = 0.0
 
     def on_epoch_end(self, model):
-        loss = 3-model.similarity('Χριστου', 'Ιησου')-model.similarity('Θεος', 'αγαπη')-model.similarity('Ιησου', 'αγαπη')
-        #this_loss = loss - self.last_loss
-        # self.last_loss = loss
-        self.tq.set_description(f'benchmark: {this_loss}')
+        loss = 4-model.wv.similarity('στρατιαν', 'δουλων')-model.wv.similarity('γνωσεως', 'καταβας')+model.wv.similarity('Χριστου', 'Ιησου')+model.wv.similarity('Θεος', 'αγαπη')
+        self.tq.set_description(f'benchmark (larger better): {loss}')
         self.epoch = next(self.progress)
-        losses[self.epoch] = this_loss
+        losses[self.epoch] = loss
 
 SIZE = 100
-EPOCHS = 2000
+EPOCHS = 800
 
 losses = np.zeros((EPOCHS,))
 
 corpus = []
 
 for fname in glob.glob('processed/*/*.txt'):
-    print(fname)
     with open(fname, 'r', encoding='utf-8') as f:
         text = f.read()
     text = text.split('\n')
     text = [[word for word in line.split() if word.lower() != 'και'.lower()] for line in text]
     corpus += text
 
-model = FastText(vector_size=SIZE, workers=16, sg=1, min_count=2, callbacks=[callback()])
-model.build_vocab(corpus_iterable=corpus)
-model.train(corpus_iterable=corpus, total_examples=len(corpus), epochs=EPOCHS)
+model = FastText(corpus, vector_size=SIZE, window=5, alpha=0.0005, negative=2, workers=16, epochs=EPOCHS, sg=1, min_count=4, callbacks=[callback()])
+del model.callbacks
 model.save(f'greek-fasttext-{SIZE}.bin')
 
-#plt.plot(losses)
-#plt.show()
+plt.plot(losses)
+plt.show()
 
 
 
